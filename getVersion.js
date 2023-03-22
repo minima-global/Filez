a1238269e61ebc49ef35a8f2e9345a09cc5c4c77
dynamitesushi <winson.hero@gmail.com>
2023-03-22 12:13:35 +0000

    updated build scripts to use version from package.json

--- getVersion.js
+++ getVersion.js
@@ -1,11 +1,2 @@
-const fs = require('fs')
-
-const dappConfPath = __dirname + '/public/dapp.conf'
-
-const data = fs.readFileSync(dappConfPath, { encoding: 'utf8', flag: 'r' })
-
-const config = JSON.parse(data)
-
-// console.log(config.version)
-
-process.stdout.write(config.version)
+const packageJson = require('./package.json');
+process.stdout.write(packageJson.version);
