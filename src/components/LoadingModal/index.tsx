e8d0149272724acc2aa0fdfc94cf3c6a4d7d310b
dynamitesushi <winson.hero@gmail.com>
2023-07-17 11:26:15 +0100

    updated filez logo and splash animation, updated the upload file ui

--- src/components/LoadingModal/index.tsx
+++ src/components/LoadingModal/index.tsx
@@ -1,8 +1,16 @@
 import { useTransition, animated } from '@react-spring/web';
 import { modalAnimation } from '../../animations';
+import loadingSpinner from '../../spinner.json';
+import Lottie from '@amelix/react-lottie';
 
 export function LoadingModal({ display, progress }: any) {
   const transition: any = useTransition(display, modalAnimation as any);
+
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: loadingSpinner,
+  };
 
   return (
     <div>
@@ -12,15 +20,27 @@ export function LoadingModal({ display, progress }: any) {
             <div className="absolute w-full h-full bg-black bg-opacity-30 left-0 top-0 z-50 flex items-center justify-center">
               <animated.div
                 style={style}
-                className="shadow-lg mx-8 w-full max-w-[240px] text-center lg:text-center font-bold bg-white rounded-lg box-shadow-md p-7 flex flex-col justify-center"
+                className="shadow-lg mx-4 w-full max-w-[320px] text-center lg:text-center font-bold bg-white rounded-lg box-shadow-md p-6 flex flex-col justify-center"
               >
-                <div className="spinner-border block mx-auto" />
+                <div className="-mt-0.5 grid grid-cols-2 flex items-center w-full">
+                  <div className="col-span-1 flex text-lg">
+                    <h1>Uploading File</h1>
+                  </div>
+                  <div className="col-span-1 flex justify-end">
+                    <div>
+                      <Lottie options={defaultOptions} height={32} width={32} />
+                    </div>
+                  </div>
+                </div>
                 {progress && (
-                  <div className="border border-slate-300 bg-slate-200 h-[25px] mt-6 relative">
-                    <div className="absolute blend z-10 right-[5px] top-0.5 font-black text-sm">{(Number(progress) * 100).toFixed(0)}%</div>
-                    <div className="bg-black absolute w-full h-[25px] transition-all origin-left" style={{ transform: `scaleX(${progress})`, left: '-1px', top: '-1px', width: 'calc(100% + 1px)' }}></div>
+                  <div className="border-2 border-black h-[36px] mt-6 relative">
+                    <div className="absolute blend z-10 left-[6px] top-[6px] font-black text-sm">{(Number(progress) * 100).toFixed(0)}%</div>
+                    <div className="bg-black absolute w-full h-[36px] transition-all origin-left" style={{ transform: `scaleX(${progress})`, left: '-1px', top: '-2px', width: 'calc(100% + 1px)' }}></div>
                   </div>
                 )}
+                <div onClick={() => window.location.reload()} className="cursor-pointer border-b w-fit mx-auto border-b-black pb-0.5 mt-6 mb-2">
+                  Cancel
+                </div>
               </animated.div>
             </div>
           )}
